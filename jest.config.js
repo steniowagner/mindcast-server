e6962ea0da8d73cf5ddb3178922fe969b3e7344e
Stenio Wagner <stenio.wagner1@gmail.com>
2019-04-17 20:17:05 -0300

    Creating the route to Update an Author

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,10 @@ module.exports = {
   coverageDirectory: '__tests__/coverage',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.js?(x)'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '<rootDir>/src/bin',
+    '<rootDir>/src/config',
+    '<rootDir>/src/app.js',
+  ],
 };
